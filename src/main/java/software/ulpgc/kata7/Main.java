573fb72d47aa2a6d6ea87ad5cc1f61ff3cec7aa8
Javier Castilla <147717877+Javier-Castilla@users.noreply.github.com>
2025-01-20 22:23:49 +0100

    Add files via upload

--- src/main/java/software/ulpgc/kata7/Main.java
+++ src/main/java/software/ulpgc/kata7/Main.java
@@ -1,7 +1,9 @@
 package software.ulpgc.kata7;
 
+import software.ulpgc.kata7.view.MainFrame;
+
 public class Main {
     public static void main(String[] args) {
-
+        new MainFrame().setVisible(true);
     }
 }
